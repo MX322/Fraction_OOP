65b36bffa4a598c3f0bf1d88075d9d1284520d8e
MX <105460091+MyNamEdoNtLike@users.noreply.github.com>
2022-12-16 18:57:06 +0200

    Добавьте файлы проекта.

---
+++ c++OOP.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class Fraction
+{
+private:
+	float numerator;
+	float denominator;
+
+public:
+	Fraction(float _numerator, float _denominator):numerator(_numerator), denominator(_denominator) {}
+	
+	float Result()
+	{
+		return numerator / denominator;
+	}
+};
+
+
+int main()
+{
+	Fraction MyFrac(5, 10);
+
+	cout << MyFrac.Result() * 2;
+}
